07319c8abaf75c1e81a81f77a751531f34fb88af
jerictilacas <shiellyllena@gmail.com>
2020-02-11 11:12:26 +0800

    update changes

---
+++ app/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+  // Load config
+  require_once 'config/config.php';
+
+  // Load helpers
+  require_once 'helpers/url.php';
+  require_once 'helpers/session.php';
+
+  // Autoload Core Libraries
+  spl_autoload_register(function ($className)
+  {
+      require_once 'libraries/'. $className . '.php';
+  });
