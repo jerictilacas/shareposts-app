15c9eb0feb20998214cab92eb1a7ee14c37f68c1
jerictilacas <shiellyllena@gmail.com>
2020-02-11 11:03:45 +0800

    define config files

---
+++ app/config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '');
+  define('DB_NAME', 'shareposts');
+
+  // App Root
+  define('APP_ROOT', dirname(dirname(__FILE__)));
+
+  // URL Root
+  define('URL_ROOT', 'http://shareapp.com');
+
+  // Site Name
+  define('SITE_NAME', 'Jerico\'s MVC');
+
+  // App Version
+  define('APP_VERSION', '1.0.0');
